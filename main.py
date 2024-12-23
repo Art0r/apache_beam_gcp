7a527f6e25449230336f6db3e0747d3115a62252
Arthur Fernandes <arthur0139@gmail.com>
2024-12-22 23:50:13 -0300

    conexao com o sqlalchemy e conector nativo do sdk gcloud

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 """
 Dataflow Job/Pipeline to retrieve data from Posgresql and transform then upload to BigQuery
 """
-from curso_apache_beam_gcp_ext_lib.pipeline_laucher import run_pipeline
+from curso_apache_beam_gcp_ext_lib.db import connection
+from sqlalchemy import text
 
-if __name__ == "__main__":
-    import sys
-    pipeline_args = sys.argv[1:]
-    run_pipeline(pipeline_args)
+with connection.connect_with_connector().connect() as conn:
+    result = conn.execute(text("SELECT * FROM users"))
+    print(result.fetchall())
+
+# from curso_apache_beam_gcp_ext_lib.pipeline_laucher import run_pipeline
+
+# if __name__ == "__main__":
+#     import sys
+#     pipeline_args = sys.argv[1:]
+#     run_pipeline(pipeline_args)
--- setup.py
+++ setup.py
@@ -9,8 +9,11 @@ setup(
     install_requires=[
         "psycopg2-binary==2.9.10",
         "google-cloud-secret-manager==2.22.0",
+        "cloud-sql-python-connector==1.15.0",
+        "SQLAlchemy==2.0.36",
         "apache-beam==2.61.0",
-        "build"
+        "pg8000==1.31.2"
+        "build",
     ],
     packages=find_packages(
         include=["curso_apache_beam_gcp_ext_lib", "curso_apache_beam_gcp_ext_lib.*"]),
